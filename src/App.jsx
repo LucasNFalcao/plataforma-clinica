b8b9d93e8ee5da41fd4ba0a243f2d2f84cf04908
Lucas Falcão <lucasfalcao23@hotmail.com>
2023-09-27 23:57:00 -0300

    Inicio do projeto e criação do componente Header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css'
+
+import Header from './componentes/header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
